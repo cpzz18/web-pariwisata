3a3006f3700aa6d16070198a21d92a37a385d6df
Zulfikar Azmi <zazmialghifari@gmail.com>
2024-10-13 13:53:33 +0700

    Save changes to README and navbar before rebase

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+            <Navbar />
+    </div>
+  );
+}
+
+export default App;
